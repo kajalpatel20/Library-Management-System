b4cbf70fcbb16bee4ee0e7e337e1cd0bb8d0a718
Kajal Patel <kajalpatel20@gmail.com>
2021-04-09 10:57:13 -0400

    made a new repo

---
+++ models/config.js
@@ -0,0 +1,30 @@
+var mysql = require('mysql');
+var connection = mysql.createConnection({
+	host: 'localhost',
+	user: 'root',
+	password: 'password',
+	port: 3306,
+	database: 'library_management_system_db',
+	insecureAuth: true
+});
+
+module.exports = {
+	executeQuery: function (sql, sqlParam, callback) {
+		if (sqlParam == null) {
+			connection.query(sql, function (error, result) {
+				if (error) {
+					console.log(error);
+				}
+				callback(result);
+			});
+		}
+		else {
+			connection.query(sql, sqlParam, function (error, result) {
+				if (error) {
+					console.log(error);
+				}
+				callback(result);
+			});
+		}
+	}
+};
---
+++ validation_rules/rules.js
@@ -0,0 +1,122 @@
+module.exports = {
+    users: {
+        create: {
+            name: {
+                required: true,
+                message: 'Name cannot be empty'
+            },
+            email: {
+                required: true,
+                type: 'email',
+                message: 'Invalid email'
+            },
+            password: {
+                required: true,
+                min: 4,
+                message: 'Invalid Password'
+            },
+            gender: {
+                required: true,
+                message: 'Must select a gender'
+            }
+        },
+        update: {
+            name: {
+                required: true,
+                message: 'Name cannot be empty'
+            },
+            email: {
+                required: true,
+                type: 'email',
+                message: 'Invalid email'
+            },
+            gender: {
+                required: true,
+                message: 'Must select a gender'
+            }
+        },
+        login: {
+            email: {
+                required: true,
+                type: 'email',
+                message: 'Invalid email'
+            },
+            password: {
+                required: true,
+                message: 'Password cannot be empty'
+            }
+        },
+        changePassword: {
+            oldPassword: {
+                required: true,
+                min: 4,
+                message: 'Invalid old password'
+            },
+            newPassword: {
+                required: true,
+                min: 4,
+                message: 'Invalid new password'
+            },
+            confirmPassword: {
+                required: true,
+                min: 4,
+                message: 'Invalid confirm password'
+            }
+        }
+    },
+
+    books: {
+        create: {
+            genre: {
+                required: true,
+                message: 'Genre cannot be empty'
+            },
+            title: {
+                required: true,
+                message: 'Title cannot be empty'
+            },
+            publisher: {
+                required: true,
+                message: 'Publisher cannot be empty'
+            },
+            author: {
+                required: true,
+                message: 'Author cannot be empty'
+            },
+            edition: {
+                required: true,
+                message: 'Invalid Edition'
+            },
+            isbn: {
+                required: true,
+                message: 'ISBN cannot be empty'
+            },
+            pages: {
+                required: true,
+                message: 'Invalid Pages'
+            }
+        },
+        request: {
+            genre: {
+                required: true,
+                message: 'Genre cannot be empty'
+            },
+            title: {
+                required: true,
+                message: 'Title cannot be empty'
+            },
+            author: {
+                required: true,
+                message: 'Author cannot be empty'
+            },
+            edition: {
+                required: true,
+                message: 'Invalid Edition'
+            },
+            isbn: {
+                required: true,
+                message: 'ISBN cannot be empty'
+            }
+        }
+    }
+};
